41d787e124a590deafee7d1d466f49b52835b712
Tomas Susanka <tsusanka@gmail.com>
2015-10-12 21:29:50 +0200

    parsing

--- RuckSackProblem.php
+++ RuckSackProblem.php
@@ -13,13 +13,22 @@ class RuckSackProblem
 
 	public function __construct($id, $size, $weight, $parameters)
 	{
+		$this->parseParameters($parameters);
+	}
+
+
+	private function parseParameters($parameters)
+	{
 		$cnt = 0;
-		foreach ($parameters as $val) {
-			$this->items[$cnt]['weight'] = $val;
-			$this->items[$cnt++]['price'] = $val;
+		for ($i = 0; $i < count($parameters); $i++) {
+			if (!($i % 2)) {
+				$this->items[$cnt]['w'] = $parameters[$i];
+
+			} else {
+				$this->items[$cnt]['p'] = $parameters[$i];
+				$cnt++;
+			}
 		}
-		var_dump($this->items);
-		exit;
 	}
 
 }
